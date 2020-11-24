8dcffd120a7bf0f0eb187cae9ceb89bed396df6b
Jeffrey <jrhicks@gmail.com>
2020-11-24 12:25:16 -0600

    Update about me

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Jeffrey Hicks',
+  subtitle: 'Learning and building in public',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Jeffrey Hicks',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Learning and building in public',
     contacts: {
       email: '',
       facebook: '#',
